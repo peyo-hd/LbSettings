14fd538ac93351556f51ac6fcfdc023c310fff47
Peter Yoon <peyo.hd@gmail.com>
2019-06-16 19:41:37 +0900

    GuidedStepActivity custom action

--- src/com/peyo/lbsettings/GuidedStepActivity.kt
+++ src/com/peyo/lbsettings/GuidedStepActivity.kt
@@ -1,10 +1,12 @@
 package com.peyo.lbsettings
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.FragmentActivity
 import androidx.leanback.app.GuidedStepSupportFragment
 import androidx.leanback.widget.GuidanceStylist
 import androidx.leanback.widget.GuidedAction
+import androidx.leanback.widget.GuidedActionsStylist
 
 class GuidedStepActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,7 @@ class GuidedStepActivity : FragmentActivity() {
     }
 
     class GuideFragment : GuidedStepSupportFragment() {
+
         override fun onCreateGuidance(savedInstanceState: Bundle?): GuidanceStylist.Guidance {
             val titleRes = activity!!.intent.getIntExtra("TitleRes", 0)
             val iconRes = activity!!.intent.getIntExtra("IconRes", 0)
@@ -23,16 +26,36 @@ class GuidedStepActivity : FragmentActivity() {
                     + "nostrud exercitation ullamco laboris nisi ut aliquip ex ea "
                     + "commodo consequat.")
             return GuidanceStylist.Guidance(resources.getString(titleRes),
-                    description, "Menu Item Guided Step", resources.getDrawable(iconRes, null))
+                    description, "Menu Item Guided Step",
+                    resources.getDrawable(iconRes, null))
         }
 
-        override fun onCreateActions(actions: MutableList<GuidedAction>, savedInstanceState: Bundle?) {
-            actions.add(GuidedAction.Builder(context).title("OK").build())
-            actions.add(GuidedAction.Builder(context).title("Cancel").build())
+        private val mAction = GuidedAction.Builder(context).title("EDIT")
+                .editable(true).build()
+
+        override fun onCreateActions(actions: MutableList<GuidedAction>,
+                                     savedInstanceState: Bundle?) {
+            actions.add(mAction)
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            openInEditMode(mAction)
+        }
+
+        override fun onCreateActionsStylist(): GuidedActionsStylist {
+            return object : GuidedActionsStylist() {
+                override fun onProvideItemLayoutId(): Int {
+                    return R.layout.text_input
+                }
+            }
         }
 
         override fun onGuidedActionClicked(action: GuidedAction?) {
             activity!!.finish()
+        }
+
+        override fun onGuidedActionEditedAndProceed(action: GuidedAction?): Long {
+            return GuidedAction.ACTION_ID_FINISH
         }
     }
 
